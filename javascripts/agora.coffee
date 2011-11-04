1ab3261462bcf69bde0b64d9b0c83d7b21845d81
Amos Wenger <amos@official.fm>
2011-11-04 02:08:25 +0100

    Oyeah alternate looks

--- javascripts/agora.coffee
+++ javascripts/agora.coffee
@@ -8,7 +8,15 @@ app = $.sammy '#main', ->
     context.$element().html "Booya"
 
   @get '#/category/:id/new', (context) ->
-    @partial('templates/new-thread.template')
+    thread_id = "chats123"
+    me  = {nickname: "BlueSky", slogan: "Win j'en ai eu ma dows", avatar: ""}
+    him = {nickname: "Sylvain", slogan: "Win j'en ai eu ma dows", avatar: ""}
+    @render('templates/thread.template', {thread: { id: thread_id }}).appendTo(@$element())
+    thread = '#' + thread_id
+    @render('templates/new-thread.template', {post: { user: him }}).appendTo(thread)
+    @render('templates/post.template', {post: { user: me, content: "Les anarchistes c'est le bien!" }}).appendTo(thread)
+    @render('templates/post.template', {post: { user: him, content: "Merde" }}).appendTo(thread)
+    @render('templates/post.template', {post: { user: me, content: "On retrouve une idée commune avec Kadoc." }}).appendTo(thread)
 
   @bind 'run', ->
 
--- javascripts/agora.js
+++ javascripts/agora.js
@@ -7,7 +7,47 @@
       return context.$element().html("Booya");
     });
     this.get('#/category/:id/new', function(context) {
-      return this.partial('templates/new-thread.template');
+      var him, me, thread, thread_id;
+      thread_id = "chats123";
+      me = {
+        nickname: "BlueSky",
+        slogan: "Win j'en ai eu ma dows",
+        avatar: ""
+      };
+      him = {
+        nickname: "Sylvain",
+        slogan: "Win j'en ai eu ma dows",
+        avatar: ""
+      };
+      this.render('templates/thread.template', {
+        thread: {
+          id: thread_id
+        }
+      }).appendTo(this.$element());
+      thread = '#' + thread_id;
+      this.render('templates/new-thread.template', {
+        post: {
+          user: him
+        }
+      }).appendTo(thread);
+      this.render('templates/post.template', {
+        post: {
+          user: me,
+          content: "Les anarchistes c'est le bien!"
+        }
+      }).appendTo(thread);
+      this.render('templates/post.template', {
+        post: {
+          user: him,
+          content: "Merde"
+        }
+      }).appendTo(thread);
+      return this.render('templates/post.template', {
+        post: {
+          user: me,
+          content: "On retrouve une idée commune avec Kadoc."
+        }
+      }).appendTo(thread);
     });
     return this.bind('run', function() {});
   });
